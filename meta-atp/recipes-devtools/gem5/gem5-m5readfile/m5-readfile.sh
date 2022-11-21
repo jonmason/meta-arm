5e00e21286ef48a09fdad8aa259fb47fc27dc0aa
Adrian Herrera <adrian.herrera@arm.com>
2022-11-21 10:43:47 -0500

    atp: decouple m5readfile from m5ops
    By making m5readfile into its own recipe, we avoid modifications to the
    m5ops recipe when using the meta-atp layer, which break the Yocto
    compatibility of the layer.
    
    Signed-off-by: Adrian Herrera <adrian.herrera@arm.com>
    Signed-off-by: Jon Mason <jon.mason@arm.com>

--- meta-atp/recipes-devtools/gem5/gem5-m5readfile/m5-readfile.sh
+++ meta-atp/recipes-devtools/gem5/gem5-m5readfile/m5-readfile.sh
@@ -3,7 +3,7 @@
 # Provides:         m5-readfile
 # Required-Start:   $all
 # Default-Start:    5
-# Description:      Enables reading any script at simulation launch time.
+# Description:      Enables reading any script at simulation launch time.
 ### END INIT INFO
 
 m5 readfile | sh
