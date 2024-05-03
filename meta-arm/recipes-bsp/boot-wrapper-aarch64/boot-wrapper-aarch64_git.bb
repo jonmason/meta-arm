jonmason/meta-arm

commit 0cec3e584dc79169ab054c0b488dffa6d3977392

arm/gem5/boot-wrapper-aarch64: Move main recipe to meta-arm
Move boot-wrapper-aarch64 recipe from meta-gem5 to meta-arm and merge the
contents of the .bb and .inc into a single recipe file.

Also, create a boot-wrapper-aarch64_%.bbappend for the gem5-arm64 machine.

Updated documentation for gem5-arm64 where meta-arm-toolchain and meta-arm
layers need to be included in the bblayers.conf.

Change-Id: Ia8193f4f0bf98069edcbf1661cb3e9b2105d12a3
Issue-Id: SCM-2080
Signed-off-by: Diego Sueiro <diego.sueiro@arm.com>
Signed-off-by: Jon Mason <jon.mason@arm.com>

meta-arm/recipes-bsp/boot-wrapper-aarch64/boot-wrapper-aarch64_git.bb
@@ -1,14 +1,20 @@
-
 SUMMARY = "Linux aarch64 boot wrapper with FDT support"
 LICENSE = "BSD"
 
-inherit autotools deploy
+LIC_FILES_CHKSUM = "file://LICENSE.txt;md5=bb63326febfb5fb909226c8e7ebcef5c"
 
-PROVIDES = "virtual/gem5-bootloader boot-wrapper-aarch64"
+SRC_URI = "git://git.kernel.org/pub/scm/linux/kernel/git/mark/boot-wrapper-aarch64.git"
+SRCREV = "fd74c8cbd0e17483d2299208cad9742bee605ca7"
+
+PV = "git${SRCPV}"
+
+S = "${WORKDIR}/git"
+
+inherit autotools deploy
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 
-COMPATIBLE_MACHINE ?= ""
+COMPATIBLE_MACHINE ?= "invalid"
 
 # Device tree to put in the image
 # by default use the standard kernel devicetree
@@ -84,4 +90,3 @@ do_deploy() {
         ${DEPLOYDIR}/linux-system.axf
 }
 addtask deploy before do_build after do_compile
-

meta-gem5/conf/layer.conf
@@ -9,5 +9,6 @@ BBFILE_COLLECTIONS += "meta-gem5"
 BBFILE_PATTERN_meta-gem5 = "^${LAYERDIR}/"
 BBFILE_PRIORITY_meta-gem5 = "5"
 
-LAYERDEPENDS_meta-gem5 = "core openembedded-layer"
+LAYERDEPENDS_meta-gem5 = "core openembedded-layer meta-arm"
 LAYERSERIES_COMPAT_meta-gem5 = "gatesgarth"
+

meta-gem5/recipes-bsp/boot-wrapper-aarch64/boot-wrapper-aarch64_%.bbappend
@@ -1,20 +1,7 @@
-
-LIC_FILES_CHKSUM = "file://LICENSE.txt;md5=bb63326febfb5fb909226c8e7ebcef5c"
-
-SRC_URI = "git://git.kernel.org/pub/scm/linux/kernel/git/mark/boot-wrapper-aarch64.git"
-
-PV = "git${SRCPV}"
-
-S = "${WORKDIR}/git"
-
-SRCREV = "fd74c8cbd0e17483d2299208cad9742bee605ca7"
-
-BPN = "boot-wrapper-aarch64"
-
-require boot-wrapper-aarch64.inc
-
 # Gem5 aarch64 support
-COMPATIBLE_MACHINE_gem5_arm64 = "gem5-arm64"
+COMPATIBLE_MACHINE_gem5-arm64 = "gem5-arm64"
+
+PROVIDES_gem5-arm64 += "virtual/gem5-bootloader"
 
 # For gem5 we use the dtb generated by gem5 directly
 DEPENDS_append_gem5-arm64 = " gem5-aarch64-dtb"
@@ -24,4 +11,3 @@ BOOT_WRAPPER_AARCH64_DEVICETREE_gem5-arm64 = "gem5-aarch64.dtb"
 DEPLOY_DEPEND_LIST ?= ""
 DEPLOY_DEPEND_LIST_gem5-arm64 = " gem5-aarch64-dtb:do_deploy"
 do_deploy[depends] += "${DEPLOY_DEPEND_LIST}"
-

commit c9c93da0dd647c9e2bfea238bdead1e0efb526ce

arm/boot-wrapper-aarch64: use https to fetch git source
Some networks limit outgoing git: traffic, so use https:.

Fixes: 0cec3e5 ("arm/gem5/boot-wrapper-aarch64: Move main recipe to meta-arm")
Signed-off-by: Ross Burton <ross.burton@arm.com>
Signed-off-by: Jon Mason <jon.mason@arm.com>

meta-arm/recipes-bsp/boot-wrapper-aarch64/boot-wrapper-aarch64_git.bb
@@ -3,7 +3,7 @@ LICENSE = "BSD-3-Clause"
 
 LIC_FILES_CHKSUM = "file://LICENSE.txt;md5=bb63326febfb5fb909226c8e7ebcef5c"
 
-SRC_URI = "git://git.kernel.org/pub/scm/linux/kernel/git/mark/boot-wrapper-aarch64.git;branch=master"
+SRC_URI = "git://git.kernel.org/pub/scm/linux/kernel/git/mark/boot-wrapper-aarch64.git;branch=master;protocol=https"
 SRCREV = "d3b1a15d18542b2086e72bfdc3fc43f454772a3b"
 
 # boot-wrapper doesn't make releases